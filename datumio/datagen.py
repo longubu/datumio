ce4322fd2ec6f465d80b74aef058274b0b11e5a5
longubu <longvho916@gmail.com>
2015-11-07 17:49:06 -0800

    Removed the need to compute um and uv separetely. Cleaned up documentation for batchgenerator. Needs work on Dataloader

--- datumio/datagen.py
+++ datumio/datagen.py
@@ -19,29 +19,23 @@ import transforms as dtf
 
 class BatchGenerator(object):
     """
-    Iterable batch fetcher with umuv & augmentations computed on-the-fly (otf).
+    Iterable batch fetcher with zmuv & augmentations computed on-the-fly (otf).
+    Requires loading the dataset onto memory beforehand.
     
     Attributes: See documentation for each to see parameters & use.
     ---------
     get_batch:
         Batch generator. Computations & augmentations are done.
 
-    set_umuv: 
-        Computes & set unit mean unit std on all of data & 
-        applies to each mini batch generation.
+    set_zmuv: 
+        Computes mean and std on all of data. Will apply zmuv
+        to each mini batch generation.
     
     set_aug_params: 
         Sets static augmentation parameters
     
     set_rng_aug_params: 
         Sets random augmentation parameter range.
-
-    set_um: 
-        Computes & set unit mean of dataset. 
-        
-    set_uv: 
-        Computes & set unit variance of dataset. 
-        
     """
     def __init__(self):
         self.aug_tf         = None
@@ -49,35 +43,37 @@ class BatchGenerator(object):
         self.mean           = None
         self.std            = None
 
-    def get_batch(self, X, y=None, batch_size=32, shuffle=True, 
+    def get_batch(self, X, labels=None, batch_size=32, shuffle=True, 
                   rng=np.random, ret_opts={'dtype': np.float32, 'chw_order': False}):     
         """ 
-        Iterable batch generator, given X data with y labels. Augmentations & umuv are 
-        computed on the fly. Use get_batch.next() to fetch batches.
+        Iterable batch generator, given X data with labels (optional). Augmentations 
+        & zmuv are computed on-the-fly. Use get_batch.next() to fetch batches.
         
         Parameters
         ---------
         X: ndarray, shape = (data, height, width, channels)
             Dataset to generate batch from.
             
-        y: ndarray, shape = (data, ) or (data, one-hot-encoded-labels), optional
-            Corresponding labels to dataset. If y is None, get_batch will 
-            only return batches of X (for datasets with no labels).
+        labels: ndarray, shape = (data, ) or (data, one-hot-encoded), optional
+            Corresponding labels to dataset. If label is None, get_batch will 
+            only return minibatches of X.
         
         batch_size: int, optional
             Sizes of minibatches to extract from X. If X % batch_size != 0,
-            then the last batch returned by get_batch is X % batch_size.
+            then the last batch returned the remainder, X % batch_size.
         
         shuffle: bool, optional
-            Whether to shuffle X before generating minibatches
+            Whether to shuffle X and labels before generating minibatches
         
-        rng: np.random.RandomState
+        rng: np.random.RandomState, optional
             Randomstate to shuffle X (if true) for reproducibility.
         
-        return_chw_order: bool, optional
-            If True, returns minibatch in dimensions (channels, height, width).
+        ret_opts: dict, optional
+            Return options. 'dtype': is the datatype to return per minibatch,
+            'chw_order': If true, will return the array in 
+            (bsize, channels, height, width), else returns in
+            (bsize, height, width, channels).
         """
-
         # parse ret_opts
         ret_dtype = ret_opts.pop('dtype', np.float32)
         ret_chw_order = ret_opts.pop('chw_order', False)
@@ -86,7 +82,7 @@ class BatchGenerator(object):
             idxs = range(len(X))
             rng.shuffle(idxs)
             X = X[idxs]
-            if y is not None: y = y[idxs]
+            if labels is not None: labels = labels[idxs]
 
         nb_batch = int(np.ceil(float(X.shape[0])/batch_size))
         for b in range(nb_batch):
@@ -99,10 +95,8 @@ class BatchGenerator(object):
             bX = []
             for i in xrange(nb_samples):
                 x = np.array(X[b*batch_size+i], dtype=np.float32)
-                if self.mean is not None:
+                if self.mean is not None and self.std is not None:
                     x -= self.mean
-                
-                if self.std is not None:
                     x /= self.std
                 
                 if self.aug_tf is not None:
@@ -117,14 +111,14 @@ class BatchGenerator(object):
             if ret_chw_order:
                 bX = bX.transpose(0, 3, 1, 2)
                 
-            if y is not None:
-                yield bX, y[b*batch_size:b*batch_size+nb_samples]
+            if labels is not None:
+                yield bX, labels[b*batch_size:b*batch_size+nb_samples]
             else:
                 yield bX
             
-    def set_umuv(self, X, axis=0):
+    def set_zmuv(self, X, axis=0):
         """
-        Computes & set unit mean unit std on entire dataset and
+        Computes mean unit std on entire dataset and
         applies to each minibatch generation.
         
         Parameters
@@ -137,32 +131,28 @@ class BatchGenerator(object):
             output shape (height, width, channels). (0,1,2) will compute
             mean across channels with output shape (3,). 
         """
-        self.set_um(X, axis=axis)
-        self.set_uv(X, axis=axis)
-
+        self.mean = X.mean(axis=axis)
+        self.std  = X.std(axis=axis)
+        
     def set_aug_params(self, input_shape, aug_params):
-        """ input_shape is shape of an image. See datumio.transforms.transform_image."""
+        """ Sets static augmentation parameters to apply to each minibatch.
+        input_shape is shape of an image. See datumio.transforms.transform_image."""
         if self.rng_aug_params: 
             raise Warning("Warning: Random augmentation is also set. Will do both!")
             
         self.aug_tf = dtf.build_augmentation_transform(input_shape, **aug_params)
     
     def set_rng_aug_params(self, rng_aug_params):
-        """ input_shape is shape of an image. See datumio.transforms.perturb_image."""
+        """ Sets random augmentation parameters to apply to each minibatch.
+        input_shape is shape of an image. See datumio.transforms.perturb_image."""
         if self.aug_tf:
             raise Warning("Warning: Regular augmentation is also set. Will do both!")
         
         self.rng_aug_params = rng_aug_params # only set parameters instead of build tf
         
-    def set_um(self, X, axis=0):
-        """ Computes & set unit mean. See `set_umuv` """
-        self.mean = X.mean(axis=axis)
-        
-    def set_uv(self, X, axis=0):
-        """ Computes & set unit std. See `set_umuv` """
-        self.std = X.std(axis=axis)
-
 def default_data_loader(dataPath):
+    """ Generic function for loading images. Supports .npy & basic PIL.Image
+    compatible extensions. dataPath(str) is the path to the image. """
     ext = os.path.basename(dataPath).split(os.path.extsep)[1]
     if ext == '.npy':
         dat = np.load(dataPath)
@@ -173,11 +163,31 @@ def default_data_loader(dataPath):
             raise IOError("default_data_loader does not recognize file type: %s"%ext)
     return dat
     
-def DataGenerator(object):
+class DataGenerator(object):
     """
-    ---
+    Iterable batch fetcher for datasets of large units with zmuv & augmentations 
+    computed on-the-fly (otf). Does not require loading the dataset beforehand.
+    
+    Attributes: See documentation for each to see parameters & use.
+    ---------
+    set_data_loader:
+        Sets the function used to load images within the minibatch.
+        
+    get_batch:
+        Batch generator. Computations & augmentations are done.
+
+    set_zmuv: 
+        Computes mean and std on all of data. Will apply zmuv
+        to each mini batch generation.
+    
+    set_aug_params: 
+        Sets static augmentation parameters
+    
+    set_rng_aug_params: 
+        Sets random augmentation parameter range.
     """
-    def __init__(self, data_loader=None, data_loader_kwargs={}):
+    
+    def __init__(self):
         self.__dict__.update(locals())
 
         self.aug_tf         = None
@@ -185,32 +195,47 @@ def DataGenerator(object):
         self.mean           = None
         self.std            = None
         
-        if data_loader is None:
-            self.data_loader = default_data_loader
-        else:
-            set_data_loader(data_loader, data_loader_kwargs=data_loader_kwargs)
-            
+        self.data_loader = default_data_loader
+        self.data_loader_kwargs = {}
+        
     def set_data_loader(self, data_loader, data_loader_kwargs={}):
+        """ Sets the function used to load images within the minibatches. The 
+        function should be of form data_loader(dataPath). See `default_data_loader`"""
         self.data_loader = data_loader
-        if data_loader_kwargs: self.data_loader_kwargs = data_loader_kwargs
+        self.data_loader_kwargs = data_loader_kwargs
        
     def set_aug_params(self, input_shape, aug_params):
-        """ input_shape is shape of an image. See datumio.transforms.transform_image."""
+        """ Sets static augmentation parameters to apply to each minibatch.
+        input_shape is shape of an image. See datumio.transforms.transform_image."""
         if self.rng_aug_params: 
             raise Warning("Warning: Random augmentation is also set. Will do both!")
             
         self.aug_tf = dtf.build_augmentation_transform(input_shape, **aug_params)
         
     def set_rng_aug_params(self, rng_aug_params):
-        """ input_shape is shape of an image. See datumio.transforms.perturb_image."""
+        """ Sets random augmentation parameters to apply to each minibatch.
+        input_shape is shape of an image. See datumio.transforms.perturb_image."""
         if self.aug_tf:
             raise Warning("Warning: Regular augmentation is also set. Will do both!")
         
         self.rng_aug_params = rng_aug_params
     
-    def set_umuv(self, mean=None, std=None, dataPaths=None, axis=0, batch_size=32):
-        """ 
-        TODO: ask dinh how to do this part better        
+    def set_zmuv(self, mean=None, std=None, dataPaths=None, axis=0, batch_size=32):
+        """
+        TODO: ask dinh for help to write this more efficiently
+        
+        Computes mean unit std on entire dataset and
+        applies to each minibatch generation.
+        
+        Parameters
+        ---------
+        mean: float,
+            
+            jfakjf
+        axis: tuple, int, 0 (default)
+            Axis to compute over. 0 will compute mean across batch with
+            output shape (height, width, channels). (0,1,2) will compute
+            mean across channels with output shape (3,). 
         """
         compute_mean = True
         compute_std = True
@@ -238,7 +263,36 @@ def DataGenerator(object):
                            to set the unit-mean unit-variance")
                                
     def get_batch(self, dataPaths, labels=None, batch_size=32, shuffle=True, 
-                  rng=np.random, ret_opts={'dtype': np.float32, 'chw_order': False}):        
+                  rng=np.random, ret_opts={'dtype': np.float32, 'chw_order': False}): 
+        """ 
+        Iterable batch generator, given X data with labels (optional). Augmentations 
+        & zmuv are computed on-the-fly. Use get_batch.next() to fetch batches.
+        
+        Parameters
+        ---------
+        X: ndarray, shape = (data, height, width, channels)
+            Dataset to generate batch from.
+            
+        labels: ndarray, shape = (data, ) or (data, one-hot-encoded), optional
+            Corresponding labels to dataset. If label is None, get_batch will 
+            only return minibatches of X.
+        
+        batch_size: int, optional
+            Sizes of minibatches to extract from X. If X % batch_size != 0,
+            then the last batch returned the remainder, X % batch_size.
+        
+        shuffle: bool, optional
+            Whether to shuffle X and labels before generating minibatches
+        
+        rng: np.random.RandomState, optional
+            Randomstate to shuffle X (if true) for reproducibility.
+        
+        ret_opts: dict, optional
+            Return options. 'dtype': is the datatype to return per minibatch,
+            'chw_order': If true, will return the array in 
+            (bsize, channels, height, width), else returns in
+            (bsize, height, width, channels).
+        """
         # parse ret_opts
         ret_dtype = ret_opts.pop('dtype', np.float32)
         ret_chw_order = ret_opts.pop('chw_order', False)
