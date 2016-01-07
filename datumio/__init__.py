d5e1481543dec198eca995272864118212a9e17f
longubu <longvho916@gmail.com>
2016-01-07 01:59:35 -0800

    Changed version to 0.1.0

--- datumio/__init__.py
+++ datumio/__init__.py
@@ -1,8 +1,13 @@
 """
 datumio: Real-time augmentation of data for inputs into deep learning models.
 ==================================
-WRITE-DESCRIPTION-HERE
+Datumio is a data loader with real-time augmentation, specifically tailored
+for inputs into image-based deep learning models (CNN).
 
+Datumio is an aggregation of other existing open source projects
+(specifically kaggle-plankton, kaggle-galaxy-zoo, keras-preprocessing-image)
+with hopes that it is simple and general enough to be used with any framework,
+from keras to caffe.
 """
 
-__version__ = '0.0.0'
+__version__ = '0.1.0'
